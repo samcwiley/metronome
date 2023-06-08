916e539ff01cf8740fd9274b4aeeee31264dcfc9
sam <samcwiley@gmail.com>
2023-06-08 13:13:03 -0400

    adding more metronome functionalities

--- clickTrack.py
+++ clickTrack.py
@@ -1,21 +1,29 @@
 from tune import Tune, Part, Medley
 
-
-#Tune(title, tune_type, tempo, numParts, transition)
+#constructing tunes using: Tune(title, tune_type, tempo, numParts, transition)
 opener = Tune("Thin Red Line", "2/4 March", 72, 4, False)
 strath1 = Tune("Munlochy Bridge", "Strathspey", 110, 2, False)
 strath2 = Tune("Highland Whisky", "Strathspey", 110, 2, False)
 jig1 = Tune("Mrs. MacPherson", "12/8 Jig", 105, 2, False)
-jig2 = Tune("Jig 2", "6/8 Jig", 105, 2, False)
+jig2 = Tune("Rocking the Baby", "6/8 Jig", 105, 2, False)
 air = Tune("Simple Gifts", "4/4 Slow March", 70, 2, False)
 reel1 = Tune("Simple Gifts Reel", "Reel", 80, 2, False)
 reel2 = Tune("Angus MacLellan", "Reel", 80, 2, False)
 reel3 = Tune("Hurlock's Reel", "Reel", 80, 2, False)
 closer = Tune("Simple Gifts Reprise", "Reel", 80, 1, False)
-
+#turn them bitches into a medley
 tune_list = [opener, strath1, strath2, jig1, jig2, air, reel1, reel2, reel3, closer]
-medley = Medley("1-Simple Gifts", tune_list, 72)
-#medley.toString()
-#   medley.make_mp3()
-opener.make_mp3()
-opener.toString()
+medley = Medley("1-Simple Gifts", tune_list, True)
+#adding two beats to bars 2, 4, 6, 8 to make part 1 of the air mixed meter
+for bar_number in [1, 3, 5, 7]:
+    for _ in range(2):
+        air.parts[0].bars[bar_number].append([1, 60/air.tempo])
+reel1.parts[0].add_off_beats(0, 2, 2)
+reel1.parts[0].accelerando(0, 2, air.tempo, 2*reel1.tempo)
+reel1.parts[0].bars[5].append([1, 60/reel1.tempo])
+#adding breaks between opener and strath, and strath and jigs using: medley.add_break(start_tune_index, num_beats, starting beat)
+medley.add_break(1, 2, 1)
+medley.add_break(3, 2, 1)
+medley.add_break(5, 3, 1)
+medley.make_mp3()
+
--- tune.py
+++ tune.py
@@ -1,13 +1,34 @@
 from pydub import AudioSegment
 from pydub.generators import Sine
 from pydub.utils import make_chunks
-import time
+import numpy as np
+
+strong_beat = 'strong_beat.wav'
+weak_beat = 'weak_beat.wav'
+break_beat = 'cymbal.wav'
+off_beat = 'off_beat.wav'
+strong = AudioSegment.from_file(strong_beat)
+weak = AudioSegment.from_file(weak_beat)
+break_beat = AudioSegment.from_file(break_beat)
+off_beat = AudioSegment.from_file(off_beat)
+
 
 class Medley:
-    def __init__(self, title, tunes, rolls):
+    def __init__(self, title, tunes, rolls=False):
         self.title = title
         self.tunes = tunes
-        self.rolls = rolls #(Zero if no rolls)
+        self.rolls = rolls
+        if self.rolls:
+            rolls = Tune("Rolls", "Rolls", self.tunes[0].tempo, 1, False)
+            self.tunes.insert(0, rolls)
+
+
+    def add_break(self, first_tune_index, num_Beats, start_Beat):
+        self.tunes[first_tune_index].parts[-1].bars[-1] = self.tunes[first_tune_index].parts[-1].bars[-1][0:-start_Beat]
+        tempo = self.tunes[first_tune_index+1].tempo
+        for i in range (num_Beats):
+            self.tunes[first_tune_index].parts[-1].bars[-1].append([2, 60/tempo])
+        
 
     def toString(self):
         print(f"{self.title} Medley. Rolls start at {self.rolls}. The tunes are: ")
@@ -17,10 +38,12 @@ class Medley:
     def make_mp3(self):
         result = AudioSegment.silent(duration=0)
         output_file = f"{self.title.replace(' ', '')}_medley_click_track.mp3"
+        
+
+
         for tune in self.tunes:
             print(f"Adding {tune.title} at time {result.duration_seconds}")
-            tune.add_tune(result)
-
+            result = tune.add_tune(result)
         result.export(output_file, format="mp3")
 
 
@@ -37,18 +60,23 @@ class Tune:
         for part_number in range(self.numParts):
             self.parts.append(Part(self.title, self.tune_type, part_number + 1, self.tempo))
     
-    def toString(self):
-        print(self.title, self.parts)
+    def toString(self, listParts=False):
+        print(f"{self.title}, a {self.numParts}-parted {self.tune_type} at {self.tempo} BPM.")
+        if listParts:
+            print("It contains parts:")
+            for part in self.parts:
+                print(f"{part.part_number}:")
+                for bar in part.bars:
+                    print(bar)
     
     def make_mp3(self):
         result = AudioSegment.silent(duration=0)
         output_file = 'clickTrack.mp3'
-        self.add_tune(result)
+        result = self.add_tune(result)
         result.export(output_file, format="mp3")
 
     def add_tune(self, track):
         for part in self.parts:
-            print(f"Adding {part.tune_name} part {part.part_number}.")
             track = part.addPart(track)
         return track
 
@@ -62,40 +90,54 @@ class Part:
         self.bars = []
 
         match tune_type:
+            case "Rolls":
+                beats_per_measure = 4
+                bars_per_part = 2
+                repeats = False
+                beat_pattern = [2, 2, 2, 4]
             case "2/4 March":
                 beats_per_measure = 2
                 bars_per_part = 8
                 repeats = True
+                beat_pattern = [0, 1]
             case "4/4 March":
                 beats_per_measure = 4
                 bars_per_part = 8
                 repeats = False
+                beat_pattern = [0, 1, 1, 1]
             case "Strathspey":
                 beats_per_measure = 4
                 bars_per_part = 8
                 repeats = False
+                beat_pattern = [0, 1, 1, 1]
             case "Reel":
                 beats_per_measure = 2
                 bars_per_part = 8
                 repeats = False
+                beat_pattern = [0, 1]
             case "6/8 Jig":
                 beats_per_measure = 2
                 bars_per_part = 8
                 repeats = True
+                beat_pattern = [0, 1]
             case "12/8 Jig":
                 beats_per_measure = 4
                 bars_per_part = 8
                 repeats = False
+                beat_pattern = [0, 1, 1, 1]
             case "4/4 Slow March":
                 beats_per_measure = 4
                 bars_per_part = 8
                 repeats = False
+                beat_pattern = [0, 1, 1, 1]
 
         for bar in range(bars_per_part):
             beats = []
-            beats.append((0, 60/self.tempo))
-            for beat in range(1, beats_per_measure):
-                beats.append((1, 60/self.tempo))
+            #beats.append([0, 60/self.tempo])
+            #for beat in range(1, beats_per_measure):
+            #    beats.append([1, 60/self.tempo])
+            for beat in beat_pattern:
+                beats.append([beat, 60/self.tempo])
             self.bars.append(beats)
 
     def toString(self):
@@ -103,18 +145,46 @@ class Part:
 
     
     def addPart(self, track):
-        strong_beat = 'strong_beat.wav'
-        weak_beat = 'weak_beat.wav'
-        strong = AudioSegment.from_file(strong_beat)
-        weak = AudioSegment.from_file(weak_beat)
         for bar in self.bars:
             for beat in bar:
                 if beat[0] == 0:
                     track += strong
+                    sound_length = len(strong)
                 if beat[0] == 1:
                     track += weak
-                silence_duration = beat[1] * 1000 - len(strong) if beat[0] == 0 else beat[1] * 1000 - len(weak)
+                    sound_length = len(weak)
+                if beat[0] == 2:
+                    track += break_beat
+                    sound_length = len(break_beat)
+                if beat[0] == 3:
+                    track += off_beat
+                    sound_length = len(off_beat)
+                if beat[0] == 4:
+                    sound_length = 0
+                silence_duration = beat[1] * 1000 - sound_length
                 if silence_duration > 0:
                     silence = AudioSegment.silent(duration=silence_duration)
                     track += silence
         return track
+    
+    def accelerando(self, start_bar, end_bar, start_tempo, end_tempo):
+        total_beats = 0
+        for bar in self.bars[start_bar:end_bar]:
+            total_beats = total_beats + len(bar)
+        beat_durations = np.linspace(60/start_tempo, 60/end_tempo, num=total_beats).tolist()
+        print(beat_durations)
+        for bar in self.bars[start_bar:end_bar]:
+            for beat in bar:
+                beat[1] = beat_durations.pop(0)
+
+    def add_off_beats(self, start_bar, end_bar, meter):
+        for bar in self.bars[start_bar:end_bar]: 
+            updated_bar = []
+            for beat in bar:
+                beat_type = beat[0]
+                beat_duration = beat[1]
+                updated_bar.append([beat_type, beat_duration / meter])
+                for _ in range(1, meter):
+                    updated_bar.append([3, beat_duration / meter])
+            bar.clear()
+            bar.extend(updated_bar)
